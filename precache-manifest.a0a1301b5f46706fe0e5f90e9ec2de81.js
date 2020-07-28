259ffb1754bd7752c68599bb9504b404f2e38130
barhamyafa <yafabarham94@gmail.com>
2020-07-28 22:40:35 +0400

    Updates

---
+++ precache-manifest.a0a1301b5f46706fe0e5f90e9ec2de81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75ee06066465cda0c974f590527dc9b",
+    "url": "/monstors/index.html"
+  },
+  {
+    "revision": "b932b813a598fed9b04e",
+    "url": "/monstors/static/css/main.969ecfeb.chunk.css"
+  },
+  {
+    "revision": "c2e7a13dfbd436f509b1",
+    "url": "/monstors/static/js/2.63a27bcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors/static/js/2.63a27bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b932b813a598fed9b04e",
+    "url": "/monstors/static/js/main.72347858.chunk.js"
+  },
+  {
+    "revision": "8ae91d6442b0e1036713",
+    "url": "/monstors/static/js/runtime-main.a3e9def4.js"
+  }
+]);
